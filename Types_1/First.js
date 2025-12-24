3367cef39563716e5994e061216df20d2b56d0a1
Amitraje9009 <rajeamit3535@gmail.com>
2025-12-24 17:40:08 +0530

    Variables

---
+++ Types_1/First.js
@@ -0,0 +1,35 @@
+
+
+/* *
+*Types of Variables
+var
+let
+Const
+
+*/
+
+var num ;
+var num = 10
+var num = 11
+console.log(num);
+
+
+let a = 10;
+// let a = 11;  //not allowed in javascript
+let b ;
+b = 10;
+console.log(b);
+
+
+// const name; //not allowed 
+const n1 = 101;
+const n2 = 101;
+
+
+// var a1 = 100
+// var a1 = 105;
+
+// console.log(a1+50);
+
+
+
