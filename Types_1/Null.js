1946f909ce6bf2a867dbd40f57b7b5ade2e2c7c7
Amitraje9009 <rajeamit3535@gmail.com>
2026-01-09 13:40:24 +0530

    -

---
+++ Types_1/Null.js
@@ -0,0 +1,22 @@
+
+
+demo();
+
+
+
+console.log(10==10);
+console.log(10=="10");   
+console.log(10==="10"); 
+
+console.log(typeof(10));
+console.log(typeof("10"));
+
+
+
+   function demo()
+   {
+    console.log("hello.....");
+    
+   }
+
+    demo();
