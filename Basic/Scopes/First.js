66ff8ef36f58aa65d54bbe1945e9932e7824c1d6
Amitraje9009 <rajeamit3535@gmail.com>
2026-01-09 14:03:35 +0530

    let

---
+++ Basic/Scopes/First.js
@@ -0,0 +1,18 @@
+
+
+if(true)
+{
+    //console.log(name);
+    
+    let name="Jon Snow";
+
+     name="Jorah Mormount";
+
+     console.log(name);
+     
+
+    }
+
+    // console.log(name);//if we try to call outside a block it give a 
+    //name is not defined
+    
