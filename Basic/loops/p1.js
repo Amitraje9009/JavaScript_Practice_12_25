93d7d0f7bf537396dee7cebe92fad5394fe836ad
Amitraje9009 <rajeamit3535@gmail.com>
2026-01-23 19:14:35 +0530

    pattern 1

---
+++ Basic/loops/p1.js
@@ -0,0 +1,12 @@
+for(v = 1; v<1000; v++){
+    if(v%111==0){
+        console.log(v);
+        
+    }
+}
+console.log("*************************");
+
+for(a=1;a<10;a++){
+    console.log(a*111);
+    
+}
