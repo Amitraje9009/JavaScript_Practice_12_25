4f766768a7a2a30c2bc0a14f4582603f95d964f5
Amitraje9009 <rajeamit3535@gmail.com>
2026-01-23 18:42:11 +0530

    Factorial

---
+++ Basic/loops/Fact.js
@@ -0,0 +1,15 @@
+
+
+for(v = 1; v <= 10; v++){
+    var sum = 1;
+    for(j = 1; j <= v; j++){
+        sum = sum * j;
+
+    }
+    console.log(sum);
+    
+}
+
+console.log("*********8");
+
+console.log(10*9*8*7*6*5*4*3*2*1);
