0a5e0407bd46307b2cf7edadbc27f90f718fde5a
Amitraje9009 <rajeamit3535@gmail.com>
2026-01-23 17:10:13 +0530

    if-else Find Grade

---
+++ Basic/Conditional_Statements/Fifth.js
@@ -0,0 +1,25 @@
+let marks = 80;
+if (marks>0 && marks<=40) {
+    console.log("You are FAIL");
+    
+} else if(marks>41 && marks<=60) {
+    console.log("C : Grade");
+    
+}else if(marks>61 && marks<=80) {
+    console.log("B : Grade");
+    
+}else if(marks>81 && marks<=100) {
+    console.log("A : Grade");
+    
+}else{
+    console.log("Invalid marks or grade");
+    
+}
+
+
+var num2=129;
+
+
+console.log(--num2/2+ ++num2+3-((--num2*2)/2));
+
+console.log(num2);
