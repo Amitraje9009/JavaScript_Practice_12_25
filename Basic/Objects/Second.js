61b035d1b8c6076ab7948250d503c910e1a67ec8
Amitraje9009 <rajeamit3535@gmail.com>
2026-02-02 08:55:10 +0530

    object

---
+++ Basic/Objects/Second.js
@@ -0,0 +1,10 @@
+let obj2 = {
+    pin:"11222334455",
+    divisions:["kondhwa","Hadapsar","Loni"],
+    display:function(){
+        console.log(this.divisions);
+    }
+}
+
+console.log(obj2);
+obj2.display;
