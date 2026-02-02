d059e62995caa5a471a1648b848b38d2401dca18
Amitraje9009 <rajeamit3535@gmail.com>
2026-02-02 09:03:49 +0530

    object arrays

---
+++ Basic/Objects/Third.js
@@ -0,0 +1,23 @@
+let obj ={
+    names:[{
+        sub:"pune",
+        post:"Baramati",
+        Area:"23456km"
+    },
+    {
+      sub:"pune2",
+        post:"haveli",
+        Area:"23456k45464km"   
+    },
+    {
+          sub:"Kolhapur",
+        post:"karvir",
+        Area:"23456k45454km"
+    }
+
+]
+}
+
+console.log(obj);
+
+console.log(obj.names[0].post);
