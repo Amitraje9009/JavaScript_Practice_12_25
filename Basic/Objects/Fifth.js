ad0e2c529f3ee82a8dc30d124d7c810a1cb74060
Amitraje9009 <rajeamit3535@gmail.com>
2026-02-02 09:10:09 +0530

    object

---
+++ Basic/Objects/Fifth.js
@@ -0,0 +1,16 @@
+let student ={
+    name:"John Snow",
+    email:"abs@gmail.com",
+    mobile:"1221212121"
+}
+
+console.log(student);
+console.log(delete student.mobile);
+console.log(student);
+console.log(delete student.mobile);
+
+console.log(delete student.name);
+
+console.log(student);
+
+
