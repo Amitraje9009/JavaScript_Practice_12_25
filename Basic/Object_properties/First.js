6bf76b1c9e5fbe76f3272da3295d33e2030b24ad
Amitraje9009 <rajeamit3535@gmail.com>
2026-02-16 10:00:49 +0530

    object

---
+++ Basic/Object_properties/First.js
@@ -0,0 +1,14 @@
+
+let obj=
+{
+    "name":"Alan joe",
+    "city":"USA",
+    "profession":"DR"
+}
+console.log(obj);
+
+
+let Obj2={};
+console.log(Obj2);
+Object.assign(Obj2,{ name: 'Alan Doo', city: 'LA LA', profession: 'DR Wr' ,"DivisoN":"Konkan"},obj);
+console.log(Obj2);
